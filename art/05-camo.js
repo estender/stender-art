59453fce9ad084bb0151766d5a362b9a14206c2f
Eric Stender <eric.stender@gmail.com>
2021-02-17 22:54:16 -0800

    Add Camouflage art

--- art/05-camo.js
+++ art/05-camo.js
@@ -1,14 +1,17 @@
 export default {
-  name: 'Camoflage',
+  name: 'Camouflage',
   iconColor: '#aaa',
   script: function (p) {
     let rows, cols, size, gridSize;
+    let xmid, ymid;
     p.setup = function () {
       p.createCanvas(p.windowWidth, p.windowHeight);
       p.background(160, 110, 20);
       p.noStroke();
       gridSize = 14;
       size = 6;
+      xmid = p.windowWidth / 2;
+      ymid = p.windowHeight / 2;
       cols = Math.floor(p.windowWidth / (gridSize + 2));
       rows = Math.floor(p.windowHeight / (gridSize + 2));
     };
@@ -20,14 +23,10 @@ export default {
         for (let j = -5; j <= cols + 5; j++) {
           let x = (j / cols) * p.windowWidth;
           let y = (i / rows) * p.windowHeight;
-          let graymod =
-            p.abs(
-              p.sin(
-                (Math.pow(i + j, 2) / (Math.pow(i, 2) + Math.pow(j, 2))) *
-                  p.millis() * 0.001
-              ) * 130
-            ) + 30;
-          graymod = p.sin(Math.pow(x * y, 2) + p.millis() * 0.0001) * 70;
+          let xdist = Math.abs(x - xmid);
+          let ydist = Math.abs(y - ymid);
+          let graymod = p.abs(p.sin(xdist + ydist + p.millis() * 0.0004) * 60);
+          graymod += p.sin(Math.pow(x * y, 2) + p.millis() * 0.0001) * 40;
           let color = p.color(10 + graymod, 30 + graymod, 10 + graymod);
 
           p.fill(color);
--- art/index.js
+++ art/index.js
@@ -2,6 +2,6 @@ import Waves from './01-waves';
 import Kaleidoscope from './02-kaleidoscope';
 import Triangles from './03-triangles';
 import Suns from './04-suns';
-import Camoflage from './05-camo';
+import Camouflage from './05-camo';
 
-export const artCollection = [Camoflage, Suns, Triangles, Kaleidoscope, Waves];
+export const artCollection = [Camouflage, Suns, Triangles, Kaleidoscope, Waves];
