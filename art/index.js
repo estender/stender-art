0c0f1cb41a55241815376c74a91b9f08e59c7aba
estender <estender@users.noreply.github.com>
2022-07-28 23:05:19 -0700

    Add rainbow clover art (#2)
    * Initial checkin of basic rainbow swirl
    
    * Refactor to classes
    
    * Rename art
    
    * Rename file

--- art/index.js
+++ art/index.js
@@ -3,5 +3,13 @@ import Kaleidoscope from './02-kaleidoscope';
 import Triangles from './03-triangles';
 import Suns from './04-suns';
 import Camouflage from './05-camo';
+import Clover from './06-clover';
 
-export const artCollection = [Camouflage, Suns, Triangles, Kaleidoscope, Waves];
+export const artCollection = [
+  Clover,
+  Camouflage,
+  Suns,
+  Triangles,
+  Kaleidoscope,
+  Waves,
+];
