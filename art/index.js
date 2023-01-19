42afeccec77c0bffba2281be0d6db425d0733cf9
estender <estender@users.noreply.github.com>
2023-01-19 15:36:08 -0800

    Add Gears aka The Grind art piece (#6)
    * Add initial draft of Gears art
    
    * Rename gears art piece

--- art/index.js
+++ art/index.js
@@ -4,6 +4,7 @@ import Triangles from './03-triangles';
 import Suns from './04-suns';
 import Camouflage from './05-camo';
 import Clover from './06-clover';
+import Gears from './06-gears';
 import TurfWar from './07-turf-war';
 import FadeToMud from './08-fade-to-mud';
 import Emojis from './09-emojis';
@@ -12,6 +13,7 @@ export const artCollection = [
   Emojis,
   FadeToMud,
   TurfWar,
+  Gears,
   Clover,
   Camouflage,
   Suns,
